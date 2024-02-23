a8ec62a314c726d992b7ff221d6c067d53efb40e
Sam Cao <sjtucaocao@gmail.com>
2024-02-23 08:00:36 +0800

    🐎 ci: Change package name

--- touch/package.cmd
+++ touch/package.cmd
@@ -1,5 +1,5 @@
 cargo b -r
 cd target\release
-del touch-0.1.0.zip
-7z a -tzip -mx9 touch-0.1.0.zip touch.exe
+del touch-windows-x86_64-0.1.0.zip
+7z a -tzip -mx9 touch-windows-x86_64-0.1.0.zip touch.exe
 cd ..\..\
--- uptime/package.cmd
+++ uptime/package.cmd
@@ -1,5 +1,5 @@
 cargo b -r
 cd target\release
-del uptime-0.1.0.zip
-7z a -tzip -mx9 uptime-0.1.0.zip uptime.exe
+del uptime-windows-x86_64-0.1.0.zip
+7z a -tzip -mx9 uptime-windows-x86_64-0.1.0.zip uptime.exe
 cd ..\..\
