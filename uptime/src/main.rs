a55410804d5abec692b7fc5c445dc0ed5ecc5245
Sam Cao <sjtucaocao@gmail.com>
2024-03-04 10:07:39 +0800

    🦄 refactor: Refactor Elapsed

--- uptime/src/main.rs
+++ uptime/src/main.rs
@@ -44,6 +44,20 @@ struct Elapsed {
 }
 
 impl Elapsed {
+  pub fn new(duration: Duration) -> Self {
+    let total_seconds = duration.as_secs();
+    let days = total_seconds / (60 * 60 * 24);
+    let hours = (total_seconds - days * 60 * 60 * 24) / (60 * 60);
+    let minutes = (total_seconds - days * 60 * 60 * 24 - hours * 60 * 60) / 60;
+    let seconds = total_seconds - days * 60 * 60 * 24 - hours * 60 * 60 - minutes * 60;
+    Elapsed {
+      days,
+      hours,
+      minutes,
+      seconds,
+    }
+  }
+
   fn pretty_print(&self) {
     let mut items: Vec<String> = vec![];
     if self.days > 0 {
@@ -91,20 +105,6 @@ impl Elapsed {
   }
 }
 
-fn build_elapsed(duration: Duration) -> Elapsed {
-  let total_seconds = duration.as_secs();
-  let days = total_seconds / (60 * 60 * 24);
-  let hours = (total_seconds - days * 60 * 60 * 24) / (60 * 60);
-  let minutes = (total_seconds - days * 60 * 60 * 24 - hours * 60 * 60) / 60;
-  let seconds = total_seconds - days * 60 * 60 * 24 - hours * 60 * 60 - minutes * 60;
-  Elapsed {
-    days,
-    hours,
-    minutes,
-    seconds,
-  }
-}
-
 fn main() {
   let args = Args::parse();
   let tick_count_64: u64;
@@ -113,13 +113,13 @@ fn main() {
   }
   let duration = Duration::new(tick_count_64 / 1000, 0);
   if args.pretty {
-    let elapsed = build_elapsed(duration);
+    let elapsed = Elapsed::new(duration);
     elapsed.pretty_print();
   } else if args.since {
     let start_date_time: DateTime<Local> = Local::now().sub(duration);
     println!("{}", start_date_time.format("%Y-%m-%d %H:%M:%S"))
   } else {
-    let elapsed = build_elapsed(duration);
+    let elapsed = Elapsed::new(duration);
     print!("{}", Local::now().format("%H:%M:%S "));
     elapsed.print();
   }
