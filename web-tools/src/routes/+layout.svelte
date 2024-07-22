5f49e584efb50cf898963c49c67d788f1f04d614
Sam Cao <sjtucaocao@gmail.com>
2024-07-22 12:37:43 +0800

    ✨ feat: Add copyright

--- web-tools/src/routes/+layout.svelte
+++ web-tools/src/routes/+layout.svelte
@@ -1,8 +1,37 @@
 <script>
-	import { Center, Container, Title, SvelteUIProvider } from '@svelteuidev/core';
+	/*
+ 	 *   Copyright (c) 2024. caoccao.com Sam Cao
+ 	 *   All rights reserved.
+
+ 	 *   Licensed under the Apache License, Version 2.0 (the "License");
+ 	 *   you may not use this file except in compliance with the License.
+ 	 *   You may obtain a copy of the License at
+
+ 	 *   http://www.apache.org/licenses/LICENSE-2.0
+
+ 	 *   Unless required by applicable law or agreed to in writing, software
+ 	 *   distributed under the License is distributed on an "AS IS" BASIS,
+ 	 *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ 	 *   See the License for the specific language governing permissions and
+ 	 *   limitations under the License.
+ 	 */
+	import { Center, Container, Text, Title, SvelteUIProvider } from '@svelteuidev/core';
 </script>
 
 <SvelteUIProvider themeObserver="light">
-	<Container size="xl"><Center><Title order={1}>web-tools</Title></Center></Container>
+	<Container size="xl">
+		<header>
+			<Center><Title order={1}>web-tools</Title></Center>
+		</header>
+	</Container>
 	<Container size="xl"><slot></slot></Container>
+	<Container size="xl" mt={10}>
+		<footer>
+			<Center>
+				<Text size="xs">
+					© Copyright 2024 Sam Cao <a href="https://www.caoccao.com/">caoccao.com</a>
+				</Text>
+			</Center>
+		</footer>
+	</Container>
 </SvelteUIProvider>
--- web-tools/src/routes/grep-it.svelte
+++ web-tools/src/routes/grep-it.svelte
@@ -1,4 +1,20 @@
 <script>
+	/*
+ 	 *   Copyright (c) 2024. caoccao.com Sam Cao
+ 	 *   All rights reserved.
+
+ 	 *   Licensed under the Apache License, Version 2.0 (the "License");
+ 	 *   you may not use this file except in compliance with the License.
+ 	 *   You may obtain a copy of the License at
+
+ 	 *   http://www.apache.org/licenses/LICENSE-2.0
+
+ 	 *   Unless required by applicable law or agreed to in writing, software
+ 	 *   distributed under the License is distributed on an "AS IS" BASIS,
+ 	 *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ 	 *   See the License for the specific language governing permissions and
+ 	 *   limitations under the License.
+ 	 */
 	import { Button, Checkbox, Flex, Stack, Textarea, TextInput } from '@svelteuidev/core';
 
 	let caseSensitiveChecked = false;
@@ -82,7 +98,7 @@
 	}
 </script>
 
-<Stack align="strech" justify="flex-start">
+<Stack align="stretch" justify="flex-start">
 	<TextInput label="Pattern *" bind:value={patternValue} on:change={onChangeGrep} />
 	<Textarea
 		label="Template"
